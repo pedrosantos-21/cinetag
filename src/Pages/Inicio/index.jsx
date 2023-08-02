ff782c383c61701f695dc90d7699570d7c777a71
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-08-02 18:21:30 -0300

    Criei o componete Card

--- src/Pages/Inicio/index.jsx
+++ src/Pages/Inicio/index.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import Banner from '../../Components/Banner'
 import Cabecalho from '../../Components/Cabecalho'
+import Card from '../../Components/Card'
 import Rodape from '../../Components/Rodape'
+import Titulo from '../../Components/Titulo'
 
 export default function Inicio() {
   return (
 
     <>
-    <Cabecalho/>
-
-    <Rodape/>
+      <Cabecalho />
+      <Banner imagem="home" />
+      <Titulo>
+        <h1> Um lugar para guardar seus videos e filmes! </h1>
+      </Titulo>
+      <Card id='1' titulo='Pedro lindão' capa='https://thecatapi.com/api/imagens/get?format=src&type=png'/>
+      <Rodape />
     </>
   )
 }
